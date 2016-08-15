8cadf8a9de6cfcc96b3d568712a5a19d8d7fe66f
akgarhwal <abhinesh.garhwal@gmail.com>
2016-08-15 13:13:57 +0530

    Create 11254-Consecutive Integers.cpp

---
+++ UVA-Solutions/11254-Consecutive Integers.cpp
@@ -0,0 +1,53 @@
+/* akgarhwal */
+// When I wrote this, only God and I understood what I was doing
+// Now, God only knows
+#include<bits/stdc++.h>
+using namespace std;
+//input
+#define si(t) scanf("%d",&t)
+#define sl(t) scanf("%lld",&t)
+#define sf(t) scanf("%f",&t)
+#define sdb(t) scanf("%lf",&t)
+#define schar(c) scanf("%c",&c)
+#define sstring(s) scanf("%s",s)
+#define ssi(a,b) scanf("%d%d",&a,&b)
+#define ssl(a,b) scanf("%lld%lld",&a,&b)
+// typedef
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+typedef set<int> si;
+typedef set<string> ss;
+typedef map<int,int> mii;
+typedef map<string,int> msi;
+//define
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define mod 1000000007
+
+//main
+int main(){
+    int N;
+    si(N);
+    while(N>=0){
+        int n= sqrt(2*N)+1e-7;
+        for(int x=n;x>0;x--){
+            int rem = ((2*N)-x*x+x)%(x*2);
+            if(rem==0 ){
+                int A = ((2*N)-x*x+x)/(x*2);
+                cout<<N<<" = "<<A<<" + ... + "<<A+x-1<<endl;
+                break;
+            }
+        }
+        si(N);
+    }
+    return 0;
+}
