3f71c92a1807ffcd8537cd22f970f3df3af9ada3
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2020-02-15 15:36:49 +0530

    Create 104_Maximum_Depth_of_Binary_Tree.cs

---
+++ LeetCode/Top 100 Liked Questions/104_Maximum_Depth_of_Binary_Tree.cs
@@ -0,0 +1,39 @@
+/** Problem Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+    }
+}
